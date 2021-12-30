0beb7fdf604f5fa4c15387d6b8fd593ab36ddfe9
Gabriel Canteiro Cruz <gabrielc896@gmail.com>
2021-12-30 15:38:07 -0300

    Initial commit

---
+++ js/sliderEquipe.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    initSlider();
+
+    function initSlider(){
+        var amt = $('.sobre-autor').length
+        var sizeContainer = 100 * amt;
+        var sizeBoxSingle = 100 / amt;
+        $('.sobre-autor').css('width',sizeBoxSingle+'%')
+        $('.scrollWraper').css('width',sizeContainer+'%')
+
+        for(var i = 0; i < amt; i++){
+            if(i == 0)
+                $('.slider-bullets').append('<span style="background-color:rgb(170,170,170);"></span>')
+        else{
+            $('slider-bullets').append('<span></span>')
+        }
+    }
+
+
+})
